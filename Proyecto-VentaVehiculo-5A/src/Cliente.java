6525018a13edaa35acd0da71ee85e6212c51edc9
yflores25400522 <yusafloresna@ittepic.edu.mx>
2026-02-13 10:33:45 -0700

    Feat: Clase Cliente Completa

---
+++ Proyecto-VentaVehiculo-5A/src/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Yulio
+ */
+public class Cliente {
+   private String rs;
+   private String rfc;
+   private String email;
+  
+   public Cliente(String rs,String rfc,String email){
+   this.rs = rs;
+   this.rfc = rfc;
+   this.email = email;
+   
+   }
+}
