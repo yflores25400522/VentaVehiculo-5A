2cfc7584335a02ae6f5620fe2d04b691faeea45e
yflores25400522 <yusafloresna@ittepic.edu.mx>
2026-02-13 10:42:15 -0700

    Feat: Clase Cliente version 2 Completa

--- Proyecto-VentaVehiculo-5A/src/Cliente.java
+++ Proyecto-VentaVehiculo-5A/src/Cliente.java
@@ -8,14 +8,22 @@
  * @author Yulio
  */
 public class Cliente {
-   private String rs;
-   private String rfc;
-   private String email;
-  
-   public Cliente(String rs,String rfc,String email){
-   this.rs = rs;
-   this.rfc = rfc;
-   this.email = email;
-   
-   }
+
+    private String rs;
+    private String rfc;
+    private String email;
+
+    public Cliente(String rs, String rfc, String email) {
+        this.rs = rs;
+        this.rfc = rfc;
+        this.email = email;
+
+    }
+
+    @Override
+    public String toString() {
+        return "rs = " + this.rs + "\n"
+                + "rfc = " + this.rfc + "\n"
+                + "email = " + this.email;
+    }
 }
